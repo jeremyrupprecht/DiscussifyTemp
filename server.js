c5c6f2ac6b21c7287b26a8e4f1947dd10f853696
Glitch (glitch-hello-website) <none>
2022-12-09 03:40:33 +0000

    🎈🎥 Checkpoint ./mainPage.html:51677067/457 ./server.js:49101200/13:51959287/10 ./style.css:51677067/534:51959287/178

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var server = require('http').createServer(app);
 const users = []
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/mainPage.html');
 })
 
 app.get('/style.css', (req, res) => {
