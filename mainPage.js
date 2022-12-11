9dfe819efc83c28de4cb044ac57baeb920c8490d
Glitch (glitch-hello-website) <none>
2022-12-11 21:38:44 +0000

    👦🍨 Checkpoint ./mainPage.js:49101200/1420 ./mainPage.html:49101200/119

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,35 @@
 let socket = io()
 
 
+socket.on("updatePosts", ({title, content, community, username}) => {
+    
+    const p2 = document.createElement("p");
+    const p2Text = document.createTextNode("posted by " + username + " in " + community);
+
+    const newDiv = document.createElement("div");
+
+    const h1 = document.createElement("H1");
+    const hText = document.createTextNode(title);
+
+    const p = document.createElement("p");
+    const pText = document.createTextNode(content);
+
+
+    h1.appendChild(hText);
+    p.appendChild(pText);
+    p2.appendChild(p2Text);
+
+    p2.style.color = "grey";
+
+    newDiv.appendChild(p2);
+    newDiv.appendChild(h1);
+    newDiv.appendChild(p);
+
+    newDiv.style.border = "3px solid #000000";
+    newDiv.style.marginTop = "10px";
+    newDiv.style.marginLeft = "180px";
+    newDiv.style.width = "80%";
+
+    document.body.appendChild(newDiv);
+    
+})
